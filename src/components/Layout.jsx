28d0b8ac7934bf36dc0512b92c55ef4f66e9c163
krollsakh <62526928+krollsakh@users.noreply.github.com>
2021-11-27 22:40:44 +0700

    NavLink

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,19 +1,32 @@
-import { Outlet, Link } from 'react-router-dom'
+import { Outlet, NavLink } from "react-router-dom";
+
+// Это для того, если хотим свое название класса для активного линка
+//  (по умолчанию можно вообще без этого,
+//  тогда просто в css прописывать класс "active"
+//  и убрать className у NavLink)
+
+const isActiveLink = ({ isActive }) => (isActive ? "active-link" : "");
 
 const Layout = () => {
   return (
     <>
       <header>
-        <Link to='/'>Home</Link>
-        <Link to='/blog'>Blog</Link>
-        <Link to='/about'>About</Link>
+        <NavLink to='/' className={isActiveLink}>
+          Home
+        </NavLink>
+        <NavLink to='/blog' className={isActiveLink}>
+          Blog
+        </NavLink>
+        <NavLink to='/about' className={isActiveLink}>
+          About
+        </NavLink>
       </header>
 
       <Outlet />
-      
+
       <footer>2021</footer>
     </>
   );
-}
+};
 
-export {Layout}
+export { Layout };
